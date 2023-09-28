1c0d2cb84de5da169e212b8d4b7cdac01fbb5d7d
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-09-28 11:26:28 +0000

    collapse functions

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,18 +2,7 @@
 """
 Prints pascal triangle
 """
-import itertools
 
-
-def compute_trianlge_seq(seq):
-    yield 1
-
-    for idx, x in enumerate(seq):
-        if idx == 0:
-            continue
-        yield (x + seq[idx - 1])
-
-    yield 1
 
 def pascal_triangle(n):
     """
@@ -25,7 +14,12 @@ def pascal_triangle(n):
     triangle = [[1]]
     for _ in range(n - 1):
         pre_row = triangle[-1]
-        cur_row = compute_trianlge_seq(pre_row)
-        triangle.append(list(cur_row))
+        cur_row = [1]
+        for idx, x in enumerate(pre_row):
+            if idx == 0:
+                continue
+            cur_row.append(x + pre_row[idx - 1])
+        cur_row.append(1)
+        triangle.append(cur_row)
     
     return triangle
