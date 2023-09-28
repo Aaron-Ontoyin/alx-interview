6a6f85a47c9d294dae0b8642348768655f6aac45
Aaron-Ontoyin <aarononto90@gmail.com>
2023-09-28 11:20:22 +0000

    pycodestyle: spaces removed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,17 +15,18 @@ def compute_trianlge_seq(seq):
 
     yield 1
 
+
 def pascal_triangle(n):
     """
     Returns a list of integers representing pascal's  triangle of size 'n'
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
     for _ in range(n - 1):
         pre_row = triangle[-1]
         cur_row = compute_trianlge_seq(pre_row)
         triangle.append(list(cur_row))
-    
+
     return triangle
