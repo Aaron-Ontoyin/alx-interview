910926bcaca0f6dddd6686a63e1ac4c4dec8d60c
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-05 18:17:18 +0000

    m

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,7 +3,7 @@
 You have n number of locked boxes in front of you.
 Each box is numbered sequentially from 0 to n - 1 and each
 box may contain keys to the other boxes.
-This module contains functions that determines if all the boxes can be opened.
+This module contains functions that determines if all the boxes can be opened
 """
 
 
