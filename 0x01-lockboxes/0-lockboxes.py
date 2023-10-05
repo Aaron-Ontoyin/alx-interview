a8d007a60ab705ffe52002be2397f7b1b6bb76e2
Aaron-Ontoyin <aarononto90@gmail.com>
2023-10-05 18:15:28 +0000

    f

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,6 +7,22 @@ This module contains functions that determines if all the boxes can be opened.
 """
 
 
+def unlock(available_keys, locked_boxes):
+    """Unlock boxes"""
+    print(locked_boxes, available_keys)
+    if len(locked_boxes) == 0:
+        return True
+
+    for box in locked_boxes:
+        box_id, box_content = box
+        if box_id in available_keys:
+            available_keys += box_content
+            locked_boxes.remove(box)
+            return unlock(available_keys, locked_boxes)
+
+    return False
+
+
 def canUnlockAll(boxes):
     """Check if all boxes can be opened"""
     if len(boxes) == 0:
@@ -15,4 +31,4 @@ def canUnlockAll(boxes):
     available_keys = boxes[0]
     locked_boxes = list(enumerate(boxes))[1:]
 
-    return True
+    return unlock(available_keys, locked_boxes)
