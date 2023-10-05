4c27527c7151708991cf8c7aa202c6d29015afa3
Aaron-Ontoyin <aarononto90@gmail.com>
2023-10-05 18:21:35 +0000

    interpreter

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 You have n number of locked boxes in front of you.
 Each box is numbered sequentially from 0 to n - 1 and each
@@ -8,26 +8,26 @@ This module contains functions that determines if all the boxes can be opened
 
 
 def unlock(available_keys, locked_boxes):
-	"""Unlock boxes"""
-	if len(locked_boxes) == 0:
-		return True
+    """Unlock boxes"""
+    if len(locked_boxes) == 0:
+        return True
 
-	for box in locked_boxes:
-		box_id, box_content = box
-		if box_id in available_keys:
-			available_keys += box_content
-			locked_boxes.remove(box)
-			return unlock(available_keys, locked_boxes)
+    for box in locked_boxes:
+        box_id, box_content = box
+        if box_id in available_keys:
+            available_keys += box_content
+            locked_boxes.remove(box)
+            return unlock(available_keys, locked_boxes)
 
-	return False
+    return False
 
 
 def canUnlockAll(boxes):
-	"""Check if all boxes can be opened"""
-	if len(boxes) == 0:
-		return False
+    """Check if all boxes can be opened"""
+    if len(boxes) == 0:
+        return False
 
-	available_keys = boxes[0]
-	locked_boxes = list(enumerate(boxes))[1:]
+    available_keys = boxes[0]
+    locked_boxes = list(enumerate(boxes))[1:]
 
-	return unlock(available_keys, locked_boxes)
+    return unlock(available_keys, locked_boxesi)
