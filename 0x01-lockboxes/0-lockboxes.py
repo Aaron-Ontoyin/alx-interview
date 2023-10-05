a4c0d30cab42ae3b21b62d32e6233b998d0b3793
Aaron-Ontoyin <aarononto90@gmail.com>
2023-10-05 17:37:13 +0000

    pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,27 +8,27 @@ This module contains functions that determines if all the boxes can be opened.
 
 
 def unlock(available_keys, locked_boxes):
-	"""Unlock boxes"""
-	print(locked_boxes, available_keys)
-	if len(locked_boxes) == 0:
-		return True
+    """Unlock boxes"""
+    print(locked_boxes, available_keys)
+    if len(locked_boxes) == 0:
+        return True
 
-	for box in locked_boxes:
-		box_id, box_content = box
-		if box_id in available_keys:
-			available_keys += box_content
-			locked_boxes.remove(box)
-			return unlock(available_keys, locked_boxes)
+    for box in locked_boxes:
+        box_id, box_content = box
+        if box_id in available_keys:
+            available_keys += box_content
+            locked_boxes.remove(box)
+            return unlock(available_keys, locked_boxes)
 
-	return False
+    return False
 
 
 def canUnlockAll(boxes):
-	"""Check if all boxes can be opened"""
-	if len(boxes) == 0:
-		return False
+    """Check if all boxes can be opened"""
+    if len(boxes) == 0:
+        return False
 
-	available_keys = boxes[0]
-	locked_boxes = list(enumerate(boxes))[1:]
+    available_keys = boxes[0]
+    locked_boxes = list(enumerate(boxes))[1:]
 
-	return unlock(available_keys, locked_boxes)
+    return unlock(available_keys, locked_boxes)
