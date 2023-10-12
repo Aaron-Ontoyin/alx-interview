cadb3f6c72b92ecc8b5d7dfda6abd019549c72b9
Aaron-Ontoyin <aarononto90@gmail.com>
2023-10-12 17:50:00 +0000

    pycodestyle, chmod, dos2unix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python3
 """
-This module contains a function that finds min operations to get n H's in a file.
-Check readme for more details
+This module contains a function that finds min operations to
+get n H's in a file. Check readme for more details
 """
 
 
 def minOperations(n):
-	if n <= 1:
-		return 0
+    """Finds the minimum number of operations required for getting n H's"""
+    if n <= 1:
+        return 0
 
-	#copyAll and Paste for first time
-	num_operations = 2
-	num_H = 2
-	clipboard_H = 1
+    # copyAll and Paste for first time
+    num_operations = 2
+    num_H = 2
+    clipboard_H = 1
 
-	# possible = True
-	while num_H != n:
-		if n % num_H == 0:
-			num_operations += 2
-			clipboard_H = num_H
-			num_H += num_H
-		else:
-			num_operations += 1
-			num_H += clipboard_H
-		# if num_H > n:
-		# 	possible = False
-		# 	break
+    # possible = True
+    while num_H != n:
+        if n % num_H == 0:
+            num_operations += 2
+            clipboard_H = num_H
+            num_H += num_H
+        else:
+            num_operations += 1
+            num_H += clipboard_H
+        # if num_H > n:
+        #     possible = False
+        #     break
 
-	return num_operations
-
-minOperations(5)
+    return num_operations
