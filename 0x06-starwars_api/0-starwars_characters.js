0925beb08c0c66b7b208c7f26516d939575f8d69
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-09 22:20:26 +0000

    add task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Please provide a Movie ID as the first argument.');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Unexpected status code:', response.statusCode);
+    process.exit(1);
+  }
+
+  const filmData = JSON.parse(body);
+
+  if (!filmData.characters || filmData.characters.length === 0) {
+    console.error('No characters found for this movie.');
+    process.exit(1);
+  }
+
+  const charactersUrls = filmData.characters;
+
+  // Fetch character names in parallel
+  charactersUrls.forEach((characterUrl) => {
+    request(characterUrl, (charError, charResponse, charBody) => {
+      if (!charError && charResponse.statusCode === 200) {
+        const characterData = JSON.parse(charBody);
+        console.log(characterData.name);
+      } else {
+        console.error('Error fetching character:', charError || `Status Code: ${charResponse.statusCode}`);
+      }
+    });
+  });
+});
