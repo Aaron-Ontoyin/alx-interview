b086aa4fb4a1957cc3be035dfab48afd0b525499
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-09 22:52:26 +0000

    da

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,35 +2,23 @@
 
 const request = require('request');
 
-async function fetchAndPrintCharacters(filmId) {
-  try {
-    const filmUrl = `https://swapi-api.hbtn.io/api/films/${filmId}`;
-    const filmResponse = await promisifiedRequest(filmUrl);
-
-    for (const characterId of filmResponse.characters) {
-      const characterResponse = await promisifiedRequest(characterId);
-      console.log(JSON.parse(characterResponse).name);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function promisifiedRequest(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(body);
-      }
-    });
-  });
-}
-
 const filmId = process.argv[2];
-if (!filmId) {
-  console.error('Please provide a Film ID as the first argument.');
-} else {
-  fetchAndPrintCharacters(filmId);
-}
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+request(url, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+
+  for (const charId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(charId, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
